ae02d3ca91decb37e0adbfc8a211f6305a332dde
10547 <10547@DESKTOP-4UL8ODR>
2019-06-30 11:45:30 +0800

    用户控制器

--- src/main/java/com/yc/controller/UserController.java
+++ src/main/java/com/yc/controller/UserController.java
@@ -1,15 +1,17 @@
 package com.yc.controller;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.yc.bean.User;
 import com.yc.service.UserService;
@@ -21,36 +23,61 @@ public class UserController {
 	UserService userService;
 	
 	@RequestMapping("logout")
-	public String  logout(HttpSession session) {
-		session.setAttribute("user", null);
+	public String  logout(HttpSession session,HttpServletRequest request,HttpServletResponse response) {
+		User u = (User) request.getSession().getAttribute("user");
+		if(u!=null) {
+			request.getSession().removeAttribute("user");
+		}
+		Cookie[] cookies=request.getCookies();
+		for(Cookie cookie:cookies){
+			if("uname".equals(cookie.getName())){
+				cookie.setMaxAge(0);			
+				response.addCookie(cookie);
+			}
+		}
 		return "redirect:index";
 	}
 	
-	@ResponseBody
+
 	@RequestMapping("doLogin")
-	public String login(String username,String password,HttpSession session,HttpServletRequest request) {
-		User user = userService.login(username, password);
-		if(user==null) {
-			 user = userService.loginByEamil(username, password);
+	public String login(Model m,String phonenum,String password,HttpSession session,String type,HttpServletResponse response) {
+		User user=null;
+		m.addAttribute("email",phonenum);
+		m.addAttribute("password",password);
+		if(type.equals("1")){
+			if(phonenum.trim().equals("") || password.trim().equals("")){
+				m.addAttribute("msg","手机号或密码不能为空");
+				return "pages/Login1";
+			}else{
+				 user = userService.login(phonenum, password);
+					if(user==null) {
+						m.addAttribute("msg","手机号或密码错误");
+						return "pages/Login1";
+					}else{
+						Cookie cookie=new Cookie("uname",user.getUsername());
+						cookie.setMaxAge(3*60);
+						cookie.setPath("/");
+						response.addCookie(cookie);
+						session.setAttribute("user", user);
+						return "redirect:index";
+					}					
+			}			
+		}	
+		if(type.equals("2")){
+			
 		}
-		session.setAttribute("user", user);
-		
-		if(user!=null) {
-			return "1";
-		}
-		return "0";
+		return "redirect:login";
 	}
 	
     @RequestMapping("/login")
-    public String log() {
-        return "pages/Login";
+    public String log(Model m,String type) {
+    	m.addAttribute("type",type);
+        return "pages/Login1";
     }
-    
-  
 	
     @RequestMapping("/reg")
     public String reg() {
-        return "pages/Register";
+        return "pages/Register1";
     }
 	
 	@RequestMapping("t")
