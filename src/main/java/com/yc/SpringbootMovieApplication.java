f609e5147bf6a6c377b6036c3139efca063af568
10547 <10547@169.254.252.184>
2019-07-04 20:37:10 +0800

    css放在static文件

--- src/main/java/com/yc/SpringbootMovieApplication.java
+++ src/main/java/com/yc/SpringbootMovieApplication.java
@@ -2,14 +2,11 @@ package com.yc;
 
 import org.mybatis.spring.annotation.MapperScan;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import com.yc.dao.MovieMapper;
-import com.yc.util.PortUtil;
 
 @SpringBootApplication
 @MapperScan("com.yc.dao")
